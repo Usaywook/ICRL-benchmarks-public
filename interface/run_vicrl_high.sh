a2ed673ade8b3ab48322405523cd5c4bac244e37
usaywook <tpdnr1360@snu.ac.kr>
2024-08-26 12:06:32 +0900

    fix command

--- interface/run_vicrl_high.sh
+++ interface/run_vicrl_high.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python train_gail.py ../config/highD_velocity_constraint/train_GAIL_highd_velocity_constraint.yaml -n 5 -s 321
-python train_gail.py ../config/highD_velocity_constraint/train_GAIL_highd_velocity_constraint.yaml -n 5 -s 456
-python train_gail.py ../config/highD_velocity_constraint/train_GAIL_highd_velocity_constraint.yaml -n 5 -s 654
-python train_gail.py ../config/highD_velocity_constraint/train_GAIL_highd_velocity_constraint.yaml -n 5 -s 666
+python train_icrl.py ../config/highD_velocity_constraint/train_VICRL_highD_velocity_constraint.yaml -n 5 -s 123
+python train_icrl.py ../config/highD_velocity_constraint/train_VICRL_highD_velocity_constraint.yaml -n 5 -s 456
+python train_icrl.py ../config/highD_velocity_constraint/train_VICRL_highD_velocity_constraint.yaml -n 5 -s 654
+python train_icrl.py ../config/highD_velocity_constraint/train_VICRL_highD_velocity_constraint.yaml -n 5 -s 666
